5123a7bcc7a51a8c879a1ae4f1b9a0494ddf391a
GannaKachalaba <111142980+GannaKachalaba@users.noreply.github.com>
2023-04-30 20:46:37 +0300

    redux- toolkit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import { App } from 'components/App';
-import './index.css';
+import { store } from './redux/store';
+import { theme } from 'constants/theme';
+import './style.css';
+// import reportWebVitals from './reportWebVitals';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
